bdd9cd086d563c7ef453eb257d56928202b4577e
Vu Ho <tuanvu.ho89@gmail.com>
2023-05-19 12:33:22 +1000

    updated featured cars, latest cars sections

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Team
+from cars_app.models import Car
 
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
-    data = {'teams': teams,}
+    featured_cars = Car.objects.filter(is_featured=True).order_by('-created_date')
+    latest_cars = Car.objects.order_by('-created_date')[:6]
+    data = {'teams': teams,
+            'featured_cars': featured_cars,
+            'latest_cars': latest_cars,}
     return render(request, 'pages/home.html', data)
 
 def about(request):
