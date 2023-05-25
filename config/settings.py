a093422384a407cc62162ccbd786aa9cfa746e81
Vu Ho <tuanvu.ho89@gmail.com>
2023-05-25 14:03:32 +1000

    implemented auto logout

--- config/settings.py
+++ config/settings.py
@@ -71,7 +71,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+# Settings for automatically logout adter Inactivity
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'home'
 
 ROOT_URLCONF = 'config.urls'
 
