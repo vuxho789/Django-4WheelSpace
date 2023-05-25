f2b6591c1e29678b3716cefa8a261462e220115b
Vu Ho <tuanvu.ho89@gmail.com>
2023-05-26 06:43:47 +1000

    removed staticfiles_dir

--- config/settings.py
+++ config/settings.py
@@ -171,9 +171,9 @@ if USE_S3:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-    STATICFILES_DIRS = [
-        BASE_DIR / 'config/static',
-    ]
+    # STATICFILES_DIRS = [
+    #     BASE_DIR / 'config/static',
+    # ]
 
     # AWS S3 Static Files Configuration
     PUBLIC_MEDIA_LOCATION = 'media'
